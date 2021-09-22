57d0b2c6cd790bffe2bd6d7c626f4e3921ca8b24
xiongyucheng <xiongyucheng@megvii.com>
2021-09-22 18:25:00 +0800

    feat: react integrate less module

---
+++ 19-react/src/demos/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.less';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      APP
+    </div>
+  );
+}
+
+export default App;
